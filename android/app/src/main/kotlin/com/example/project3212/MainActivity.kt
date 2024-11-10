f99c459df59d64ab4445f2ec6a6fb7d029da32a2
Ibshar <cse_182210012101018@lus.ac.bd>
2024-11-10 20:30:40 +0600

    dependencies updated

---
+++ android/app/src/main/kotlin/com/example/project3212/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project3212
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:taskmanager/app.dart';
+
+void main() {
+  runApp(const Taskmanagerapp());
+}
